177c1727c430884c5aa7a50bb61e1b46b0f70e05
Patrick <patryk.foryszewski@gmail.com>
2022-03-21 17:58:30 +0100

    Add settings file

--- publish_topic.py
+++ publish_topic.py
@@ -1,26 +1,19 @@
 import paho.mqtt.client as mqtt
-from random import randrange, uniform
+from random import uniform
 import time
 
-TOPIC = "TEMPERATURE_Qu4e"
-BROKERS = [
-    "iot.eclipse.org",
-    "mqtt.eclipseprojects.io",
-    "broker.hivemq.com",
-    "127.0.0.1"
-]
-BROKER = BROKERS[3]
-PORT = 1833
+import settings
+
 
 if __name__ == "__main__":
-    mqtt_broker = BROKER
+    mqtt_broker = settings.BROKER
 
     client = mqtt.Client("Temperature_Inside")
     client.connect(mqtt_broker)
 
-    print(f"PUBLISH DATA {TOPIC}")
+    print(f"PUBLISH DATA {settings.TOPIC}")
     for _ in range(35):
         rand_number = uniform(20.0, 21.0)
-        client.publish(TOPIC, rand_number)
-        print(f"Just published {rand_number} to topic {TOPIC} on {BROKER}")
+        client.publish(settings.TOPIC, rand_number)
+        print(f"Just published {rand_number} to topic {settings.TOPIC} on {settings.BROKER}")
         time.sleep(1)
---
+++ settings.py
@@ -0,0 +1,9 @@
+TOPIC = "TEMPERATURE_Qu4e"
+BROKERS = [
+    "iot.eclipse.org",
+    "mqtt.eclipseprojects.io",
+    "broker.hivemq.com",
+    "127.0.0.1"
+]
+BROKER = BROKERS[3]
+PORT = 1833
