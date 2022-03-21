9104abe018cbb6ec1d316b84ad090d17132adf25
Patrick <patryk.foryszewski@gmail.com>
2022-03-21 18:06:46 +0100

    Format settings

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 TOPIC = "TEMPERATURE_Qu4e"
+
 BROKERS = [
     "iot.eclipse.org",
     "mqtt.eclipseprojects.io",
     "broker.hivemq.com",
     "127.0.0.1"
 ]
+
 BROKER = BROKERS[3]
+
 PORT = 1833
+
