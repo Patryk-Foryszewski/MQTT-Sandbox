54aa1bf31a8830edcedcb0b502ed05dd262d7d6e
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-03-22 15:41:44 +0100

    Remove get_logger function

--- loggers.py
+++ loggers.py
@@ -7,27 +7,6 @@ def get_logger_filename(name: str) -> str:
     return f"{settings.LOGGER_DIR}/{name}.log"
 
 
-def get_logger(
-        name: str = "LOGGING",
-        file: str = None,
-        log_format: str = settings.LOGGER_FORMAT,
-        level: int = settings.LOGGER_LEVEL,
-        ):
-
-    file = get_logger_filename(file) if file else settings.LOGGER_FILE
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    formatter = logging.Formatter(log_format)
-    file_handler = logging.FileHandler(file)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-    if True:  # to be sys.args console setting
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        logger.addHandler(stream_handler)
-    return logger
-
-
 class Logger(Observer):
 
     def __init__(self,
