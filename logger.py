5a465307338d70938d57d9b513b635f610f358a7
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-03-22 11:04:23 +0100

    Add Publisher class and events to fit observer pattern

--- logger.py
+++ logger.py
@@ -1,5 +1,6 @@
 import logging
 import settings
+from events import Observer, Subject
 
 
 def get_logger_filename(name: str) -> str:
@@ -25,3 +26,29 @@ def get_logger(
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
     return logger
+
+
+class Logger(Observer):
+
+    def __init__(self,
+                 name: str = "LOGGING",
+                 file: str = None,
+                 log_format: str = settings.LOGGER_FORMAT,
+                 level: int = settings.LOGGER_LEVEL,
+                 ):
+        file = get_logger_filename(file) if file else settings.LOGGER_FILE
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        formatter = logging.Formatter(log_format)
+        file_handler = logging.FileHandler(file)
+        file_handler.setFormatter(formatter)
+        if True:  # to be sys.args console setting
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            self.logger.addHandler(stream_handler)
+        self.logger.addHandler(file_handler)
+
+    def update(self, subject: Subject) -> None:
+        self.logger(subject.message)
+        # if subject._state < 3:
+        #     print("ConcreteObserverA: Reacted to the event")
--- publish_topic.py
+++ publish_topic.py
@@ -3,19 +3,50 @@ from random import uniform
 import time
 
 import settings
-from logger import get_logger
+from events import Subject, Observer
+from random import randrange
+from typing import List
+# from logger import get_logger
+#
+# log = get_logger(name="PUBLISH")
+#
+# if __name__ == "__main__":
+#     mqtt_broker = settings.BROKER
+#
+#     client = mqtt.Client("Temperature_Inside")
+#     client.connect(mqtt_broker)
+#     log.info(f"START PUBLISHING DATA {settings.TOPIC}")
+#
+#     for _ in range(35):
+#         rand_number = uniform(20.0, 21.0)
+#         client.publish(settings.TOPIC, rand_number)
+#         log.info(f"PUBLISHED {rand_number} TO TOPIC {settings.TOPIC} ON {settings.BROKER}")
+#         time.sleep(1)
 
-log = get_logger(name="PUBLISH")
 
-if __name__ == "__main__":
-    mqtt_broker = settings.BROKER
+class Publisher(Subject):
+    message: str = ""
 
-    client = mqtt.Client("Temperature_Inside")
-    client.connect(mqtt_broker)
-    log.info(f"START PUBLISHING DATA {settings.TOPIC}")
+    _observers: List[Observer] = []
+    """
+    List of subscribers. In real life, the list of subscribers can be stored
+    more comprehensively (categorized by event type, etc.).
+    """
+    def __init__(self, client):
+        self.client = client
 
-    for _ in range(35):
-        rand_number = uniform(20.0, 21.0)
-        client.publish(settings.TOPIC, rand_number)
-        log.info(f"PUBLISHED {rand_number} TO TOPIC {settings.TOPIC} ON {settings.BROKER}")
-        time.sleep(1)
+    def attach(self, observer: Observer) -> None:
+        self._observers.append(observer)
+
+    def detach(self, observer: Observer) -> None:
+        self._observers.remove(observer)
+
+    def notify(self) -> None:
+
+        for observer in self._observers:
+            observer.update(self)
+
+    def publish_temperature(self, temperature):
+        self.client.publish(settings.TOPIC, temperature)
+        self.message = f"PUBLISHED {temperature} TO TOPIC {settings.TOPIC} ON {settings.BROKER}"
+        self.notify()
