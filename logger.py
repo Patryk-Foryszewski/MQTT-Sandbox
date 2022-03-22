9eafc72d06667d1cdbce867c853d542137fda068
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-03-22 08:20:23 +0100

    Add function annotations

--- logger.py
+++ logger.py
@@ -20,7 +20,7 @@ def get_logger(
     file_handler = logging.FileHandler(file)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
-    if True:
+    if True:  # to be sys.args console setting
         stream_handler = logging.StreamHandler()
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
