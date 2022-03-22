3933c160875de8a606a74c79daaa423a681b063f
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-03-22 14:10:58 +0100

    Add main file

---
+++ main.py
@@ -0,0 +1,42 @@
+from publish_topic import Publisher
+from subscribe_topic import Subscriber
+import settings
+import sched, time
+from datetime import datetime
+from random import uniform
+from loggers import Logger
+
+
+if __name__ == "__main__":
+    data = {
+        "topic": settings.TOPIC,
+        "broker": settings.BROKER
+    }
+
+    subscriber = Subscriber(client_name="smartphone", **data)
+    subscriber_logger = Logger(
+        name="SUBSCRIBER",
+        file="subscriber"
+    )
+    subscriber.attach(subscriber_logger)
+    # subscriber.client.on_message = subscriber.on_message
+
+    publisher = Publisher(client_name="temperature_inside", **data)
+    publisher_logger = Logger(
+        name="PUBLISHER",
+        file="publisher"
+    )
+    publisher.attach(publisher_logger)
+    time_start = datetime.now()
+    s = sched.scheduler(time.time, time.sleep)
+
+    def i_am_publishing(sc):
+        value = uniform(20.0, 21.0)
+        publisher.publish(value)
+        delta = datetime.now() - time_start
+        if delta.total_seconds() < settings.PLAY_TIME:
+            s.enter(settings.TIME_INTERVAL, 0, i_am_publishing, (sc,))
+
+    i_am_publishing(s)
+    s.run()
+    subscriber.stop()
